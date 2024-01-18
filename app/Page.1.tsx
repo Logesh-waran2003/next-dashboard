7163df38325cf60262b5bd882f1166d23e7062f1
Logesh <Logeshwaransrini@gamil.com>
2024-01-19 02:03:27 +0530

    added /download routes

--- app/Page.1.tsx
+++ app/Page.1.tsx
@@ -1,6 +1,7 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
